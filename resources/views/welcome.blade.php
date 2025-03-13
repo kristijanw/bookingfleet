8e4fd9ff825a895d4d1c86284449aad2524b756b
kristijanw <freelance.kristijan@gmail.com>
2025-03-13 14:19:53 +0100

    test pwa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,12 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <meta name="apple-mobile-web-app-status-bar" content="#01d679">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+
         <title>Booking Fleet</title>
+
+        <link rel="manifest" href="/manifest.json">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -13,6 +18,38 @@
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Poetsen+One&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+        <link rel="apple-touch-icon" sizes="16x16" href="/img/AppImages/ios/16.png">
+        <link rel="apple-touch-icon" sizes="20x20" href="/img/AppImages/ios/20.png">
+        <link rel="apple-touch-icon" sizes="29x29" href="/img/AppImages/ios/29.png">
+        <link rel="apple-touch-icon" sizes="32x32" href="/img/AppImages/ios/32.png">
+        <link rel="apple-touch-icon" sizes="40x40" href="/img/AppImages/ios/40.png">
+        <link rel="apple-touch-icon" sizes="50x50" href="/img/AppImages/ios/50.png">
+        <link rel="apple-touch-icon" sizes="57x57" href="/img/AppImages/ios/57.png">
+        <link rel="apple-touch-icon" sizes="58x58" href="/img/AppImages/ios/58.png">
+        <link rel="apple-touch-icon" sizes="60x60" href="/img/AppImages/ios/60.png">
+        <link rel="apple-touch-icon" sizes="64x64" href="/img/AppImages/ios/64.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="/img/AppImages/ios/72.png">
+        <link rel="apple-touch-icon" sizes="76x76" href="/img/AppImages/ios/76.png">
+        <link rel="apple-touch-icon" sizes="80x80" href="/img/AppImages/ios/80.png">
+        <link rel="apple-touch-icon" sizes="87x87" href="/img/AppImages/ios/87.png">
+        <link rel="apple-touch-icon" sizes="100x100" href="/img/AppImages/ios/100.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="/img/AppImages/ios/114.png">
+        <link rel="apple-touch-icon" sizes="120x120" href="/img/AppImages/ios/120.png">
+        <link rel="apple-touch-icon" sizes="128x128" href="/img/AppImages/ios/128.png">
+        <link rel="apple-touch-icon" sizes="144x144" href="/img/AppImages/ios/144.png">
+        <link rel="apple-touch-icon" sizes="152x152" href="/img/AppImages/ios/152.png">
+        <link rel="apple-touch-icon" sizes="167x167" href="/img/AppImages/ios/167.png">
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/AppImages/ios/180.png">
+        <link rel="apple-touch-icon" sizes="192x192" href="/img/AppImages/ios/192.png">
+        <link rel="apple-touch-icon" sizes="256x256" href="/img/AppImages/ios/256.png">
+        <link rel="apple-touch-icon" sizes="512x512" href="/img/AppImages/ios/512.png">
+        <link rel="apple-touch-icon" sizes="1024x1024" href="/img/AppImages/ios/1024.png">
+
+        <link href="/img/AppImages/ios/1024.png" sizes="1024x1024" rel="apple-touch-startup-image">
+        <link href="/img/AppImages/ios/512.png" sizes="512x512" rel="apple-touch-startup-image">
+        <link href="/img/AppImages/ios/256.png" sizes="256x256" rel="apple-touch-startup-image">
+        <link href="/img/AppImages/ios/192.png" sizes="192x192" rel="apple-touch-startup-image">
 
         <!-- Styles / Scripts -->
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
@@ -208,5 +245,7 @@
                 <p class="text-[#004972] font-medium work-sans text-sm">Created by: PROSPEKT</p>
             </div>
         </footer>
+
+        @vite(['resources/js/app.js'])
     </body>
 </html>
