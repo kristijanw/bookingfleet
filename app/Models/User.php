b61d2db131da885fc270aa3ef3fa4aa78fb24d9f
kristijanw <freelance.kristijan@gmail.com>
2025-03-17 15:32:06 +0100

    update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,12 @@
 namespace App\Models;
 
 use DirectoryTree\Authorization\Traits\Authorizable;
-use Filament\Models\Contracts\FilamentUser;
-use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable
 {
     use HasFactory;
     use Notifiable;
@@ -41,10 +39,5 @@ class User extends Authenticatable implements FilamentUser
     public function excursion(): HasMany
     {
         return $this->hasMany(Excursion::class);
-    }
-
-    public function canAccessPanel(Panel $panel): bool
-    {
-        return true;
     }
 }
